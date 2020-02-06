c8f8d4401e0056bec4bb73ec51a6053eb1da624b
Suveb <ansarisuveb@gmail.com>
2020-02-06 16:50:50 +0530

    autoencoder changes

--- app.py
+++ app.py
@@ -4,7 +4,7 @@
 @author: Shoaib
 """
 
-from flask import Flask,jsonify,request
+from flask import Flask,jsonify,request,render_template
 from flask_cors import CORS, cross_origin
 import numpy as np
 from tensorflow.keras.models import load_model
@@ -30,12 +30,12 @@ app.config['CORS_HEADERS'] = 'Content-Type'
 
 sepsis_model = load_model('model.h5')
 
-cols=['HR',  'O2Sat',  'Temp',  'SBP',  'MAP',  'DBP',  'Resp',  'EtCO2',
-       'BaseExcess' ,  'HCO3' ,  'FiO2' ,  'pH' ,  'PaCO2' ,  'SaO2' ,  'AST' ,  'BUN',
-       'Alkalinephos' ,  'Calcium' ,  'Chloride' ,  'Creatinine' ,  'Bilirubin_direct' ,
-       'Glucose' ,  'Lactate',  'Magnesium' ,  'Phosphate' ,  'Potassium' ,
-       'Bilirubin_total' ,  'TroponinI' ,  'Hct' ,  'Hgb' ,  'PTT' ,  'WBC' ,
-       'Fibrinogen' ,  'Platelets' ,  'Age' ,  'Gender' ,  'Unit1' ,  'Unit2' ,
+cols=['HR',  'O2Sat',  'Temp',  'SBP',  'MAP',  'DBP',  'Resp',
+       'BaseExcess' ,  'HCO3' ,  'FiO2' ,  'pH' ,  'PaCO2' ,  'AST' ,  'BUN',
+        'Calcium' ,  'Chloride' ,
+       'Glucose' ,  'Magnesium' , 'Potassium' ,
+       'Hct' ,  'Hgb' ,  'WBC' ,
+       'Age' ,  'Gender' ,  'Unit1' ,  'Unit2' ,
        'HospAdmTime' ,  'ICULOS' ]
  
 
@@ -44,6 +44,7 @@ cols=['HR',  'O2Sat',  'Temp',  'SBP',  'MAP',  'DBP',  'Resp',  'EtCO2',
 def server_status():
     
     return jsonify({ "status" : "online" })
+
 
 @app.route('/prediction', methods=['POST'] )
 @cross_origin()
@@ -54,6 +55,7 @@ def prediction():
     result = return_prediction(sepsis_model,body)
     
     return jsonify({ "prediction" : str(result) })
+
 
 if __name__ == '__main__':
     app.run(debug=True)
